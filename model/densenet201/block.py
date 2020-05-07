519a7b3896d914f6c28e18de8d1ecd520bbdc0bd
hakanahmad <58800253+hakanahmad@users.noreply.github.com>
2020-05-07 21:44:04 +0700

    Update block.py

--- model/densenet201/block.py
+++ model/densenet201/block.py
@@ -10,10 +10,10 @@ import matplotlib.pyplot as plt
 def conv_block(layer,kernel_array,stride_array,filter_array):
     x1 = BatchNormalization()(layer)
     x1 = ReLU()(x1)
-    x1 = Conv2D(kernel_size=kernel_array[0],strides=stride_array[0],filters=filter_array[0])(x1)
+    x1 = Conv2D(kernel_size=kernel_array[0],strides=stride_array[0],filters=filter_array[0],padding='same')(x1)
     x1 = BatchNormalization()(x1)
     x1 = ReLU()(x1)
-    x1 = Conv2D(kernel_size=kernel_array[1],strides=stride_array[1],filters=filter_array[1])(x1)
+    x1 = Conv2D(kernel_size=kernel_array[1],strides=stride_array[1],filters=filter_array[1],padding='same')(x1)
     x = Concatenate()([layer,x1])
     return x
 
@@ -25,6 +25,6 @@ def dense_block(layer,blocks):
 def transition_block(layer):
     x = BatchNormalization()(layer)
     x = ReLU()(x)
-    x = Conv2D(kernel_size=1,strides=2,filters=32)(x)
+    x = Conv2D(kernel_size=1,strides=2,filters=32,padding='same')(x)
     x = AveragePooling2D()(x)
     return x
