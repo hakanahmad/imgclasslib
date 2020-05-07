b869b3d179b0020392599cf75664558a42fb98aa
hakanahmad <58800253+hakanahmad@users.noreply.github.com>
2020-05-07 22:52:10 +0700

    Add files via upload

---
+++ model/densenet402/__init__.py
@@ -0,0 +1,32 @@
+import keras
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import regularizers
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization, Add, AveragePooling2D, Flatten, Dense, MaxPooling2D, Dropout, Concatenate, Average, ZeroPadding2D, LeakyReLU, GlobalAveragePooling2D
+import pickle
+import matplotlib.pyplot as plt
+from imgclasslib.model.densenet201.block import *
+
+def create_densenet201(IMG_SIZE,num_categories=4):
+    inputs = Input(shape=(IMG_SIZE,IMG_SIZE,3))
+    x = ZeroPadding2D(padding=(3,3))(inputs)
+    x = Conv2D(kernel_size=7,strides=2,filters=64)(x)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+    x = ZeroPadding2D(padding=(1,1))(x)
+    x = MaxPooling2D(pool_size=3,strides=2)(x)
+    x = dense_block(x,12)
+    x = transition_block(x)
+    x = dense_block(x,24)
+    x = transition_block(x)
+    x = dense_block(x,96)
+    x = transition_block(x)
+    x = dense_block(x,64)
+    x = BatchNormalization()(x)
+    x = ReLU()(x)
+    x = GlobalAveragePooling2D()(x)
+    outputs = Dense(num_categories,activation='softmax')(x)
+    model = Model(inputs,outputs)
+    model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+    return model
---
+++ model/densenet402/block.py
@@ -0,0 +1,30 @@
+import keras
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import regularizers
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization, Add, AveragePooling2D, Flatten, Dense, MaxPooling2D, Dropout, Concatenate, Average, ZeroPadding2D, LeakyReLU
+import pickle
+import matplotlib.pyplot as plt
+
+def conv_block(layer,kernel_array,stride_array,filter_array):
+    x1 = BatchNormalization()(layer)
+    x1 = ReLU()(x1)
+    x1 = Conv2D(kernel_size=kernel_array[0],strides=stride_array[0],filters=filter_array[0])(x1)
+    x1 = BatchNormalization()(x1)
+    x1 = ReLU()(x1)
+    x1 = Conv2D(kernel_size=kernel_array[1],strides=stride_array[1],filters=filter_array[1])(x1)
+    x = Concatenate()([layer,x1])
+    return x
+
+def dense_block(layer,blocks):
+    for i in range(blocks):
+        x = conv_block(layer,[1,3],[1,1],[4*32,32])
+    return x
+
+def transition_block(layer):
+    x = BatchNormalization()(layer)
+    x = ReLU()(x)
+    x = Conv2D(kernel_size=1,strides=2,filters=32)(x)
+    x = AveragePooling2D(pool_size=2,strides=2)(x)
+    return x
