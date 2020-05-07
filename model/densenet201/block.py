03d88c4a98f0c99efcc713ffd5c5775b0677cb0a
hakanahmad <58800253+hakanahmad@users.noreply.github.com>
2020-05-07 21:15:37 +0700

    Update block.py

--- model/densenet201/block.py
+++ model/densenet201/block.py
@@ -14,7 +14,7 @@ def conv_block(layer,kernel_array,stride_array,filter_array):
     x1 = BatchNormalization()(x1)
     x1 = ReLU()(x1)
     x1 = Conv2D(kernel_size=kernel_array[1],strides=stride_array[1],filters=filter_array[1])(x1)
-    x = Concatenate(axis=-1)([x,x1])
+    x = Concatenate(axis=-1)([layer,x1])
     return x
 
 def dense_block(layer,blocks):
