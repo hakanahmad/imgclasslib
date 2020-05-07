62e3ac2ba05231565cce02daa7bfffe0d4976c5c
hakanahmad <58800253+hakanahmad@users.noreply.github.com>
2020-05-07 23:06:17 +0700

    Update block.py

--- model/densenet402/block.py
+++ model/densenet402/block.py
@@ -10,16 +10,16 @@ import matplotlib.pyplot as plt
 def conv_block(layer,kernel_array,stride_array,filter_array):
     x1 = BatchNormalization()(layer)
     x1 = ReLU()(x1)
-    x1 = Conv2D(kernel_size=kernel_array[0],strides=stride_array[0],filters=filter_array[0])(x1)
+    x1 = Conv2D(kernel_size=kernel_array[0],strides=stride_array[0],filters=filter_array[0],padding='same')(x1)
     x1 = BatchNormalization()(x1)
     x1 = ReLU()(x1)
-    x1 = Conv2D(kernel_size=kernel_array[1],strides=stride_array[1],filters=filter_array[1])(x1)
+    x1 = Conv2D(kernel_size=kernel_array[1],strides=stride_array[1],filters=filter_array[1],padding='same')(x1)
     x = Concatenate()([layer,x1])
     return x
 
-def dense_block(layer,blocks):
+def dense_block(x,blocks):
     for i in range(blocks):
-        x = conv_block(layer,[1,3],[1,1],[4*32,32])
+        x = conv_block(x,[1,3],[1,1],[4*32,32])
     return x
 
 def transition_block(layer):
