9b808f8373bab9928a5bdd89a2c0700de2a03eff
hakanahmad <58800253+hakanahmad@users.noreply.github.com>
2020-05-07 22:23:35 +0700

    Update __init__.py

--- model/densenet201/__init__.py
+++ model/densenet201/__init__.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.keras import regularizers
 from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization, Add, AveragePooling2D, Flatten, Dense, MaxPooling2D, Dropout, Concatenate, Average, ZeroPadding2D, LeakyReLU
+from tensorflow.keras.layers import Input, Conv2D, ReLU, BatchNormalization, Add, AveragePooling2D, Flatten, Dense, MaxPooling2D, Dropout, Concatenate, Average, ZeroPadding2D, LeakyReLU, GlobalAveragePooling2D
 import pickle
 import matplotlib.pyplot as plt
 from imgclasslib.model.densenet201.block import *
@@ -25,7 +25,7 @@ def create_densenet201(IMG_SIZE,num_categories=4):
     x = dense_block(x,32)
     x = BatchNormalization()(x)
     x = ReLU()(x)
-    x = AveragePooling2D(pool_size=7)(x)
+    x = GlobalAveragePooling2D(pool_size=7)(x)
     x = Flatten()(x)
     outputs = Dense(num_categories,activation='softmax')(x)
     model = Model(inputs,outputs)
