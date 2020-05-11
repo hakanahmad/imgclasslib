df98e7637e5e5f6e2a0cf73cdd4ba9bdafcda17b
hakanahmad <58800253+hakanahmad@users.noreply.github.com>
2020-05-11 20:22:35 +0700

    Update __init__.py

--- model/resnet50/__init__.py
+++ model/resnet50/__init__.py
@@ -51,5 +51,9 @@ def create_resnet50(IMG_SIZE,num_categories=4):
     x = Flatten()(x)
     outputs = Dense(num_categories,activation='softmax')(x)
     model = Model(inputs,outputs)
-    model.compile(optimizer='adam',loss='sparse_categorical_crossentropy',metrics=['accuracy'])
+    if num_categories == 2:
+        loss = 'binary_crossentropy'
+    elif num_categories > 2:
+        loss = 'sparse_categorical_crossentropy'
+    model.compile(optimizer='adam',loss=loss,metrics=['accuracy'])
     return model
